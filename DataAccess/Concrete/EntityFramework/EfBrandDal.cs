d174ec53cf6ea2b900c7f0d3bedf15c9550b7426
Gizem Kübra Yaman <gizem.2406@gmail.com>
2021-03-09 23:10:14 +0300

    update

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.EntityFramework
@@ -10,27 +12,50 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Brand entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Brand entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Brand Get(Expression<Func<Brand, bool>> filter)
         {
-            return null;
+            using (CarDBContext context = new CarDBContext())
+            {
+                return context.Set<Brand>().SingleOrDefault(filter);
+            }
         }
 
         public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
         {
-            return null;
+            using (CarDBContext context = new CarDBContext())
+            {
+                return filter == null 
+                    ? context.Set<Brand>().ToList() 
+                    : context.Set<Brand>().Where(filter).ToList();
+            }
         }
 
         public void Update(Brand entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 
--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.EntityFramework
@@ -10,27 +12,50 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Color entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Color entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Color Get(Expression<Func<Color, bool>> filter)
         {
-            return null;
+            using (CarDBContext context = new CarDBContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+            }
         }
 
         public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
         {
-            return null;
+            using (CarDBContext context = new CarDBContext())
+            {
+                return filter == null 
+                    ? context.Set<Color>().ToList() 
+                    : context.Set<Color>().Where(filter).ToList();
+            }
         }
 
         public void Update(Color entity)
         {
-            
+            using (CarDBContext context = new CarDBContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 
