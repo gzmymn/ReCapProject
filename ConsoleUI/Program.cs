6a0289f2be2a358b9035b3ce2849e2c89b01dafc
Gizem Kübra Yaman <gizem.2406@gmail.com>
2021-03-16 04:16:37 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,6 +19,37 @@ namespace ConsoleUI
             //CarDetailTest();
             //BrandGetAllTest();
 
+            //UserAddTest();
+
+            //BrandAddTest();
+
+            RentalAddTest();
+
+        }
+
+        private static void RentalAddTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            rentalManager.Add(new Rental { CarId = 1, CustomerId = 1, RentDate = DateTime.Now, ReturnDate = DateTime.Now.AddDays(3) });
+            rentalManager.Add(new Rental { CarId = 2, CustomerId = 1, RentDate = DateTime.Now, ReturnDate = DateTime.Now.AddDays(4) });
+            rentalManager.Add(new Rental { CarId = 3, CustomerId = 2, RentDate = DateTime.Now, ReturnDate = DateTime.Now.AddDays(7) });
+            rentalManager.Add(new Rental { CarId = 4, CustomerId = 2, RentDate = DateTime.Now, ReturnDate = DateTime.Now.AddDays(2) });
+            rentalManager.Add(new Rental { CarId = 5, CustomerId = 2, RentDate = DateTime.Now });
+        }
+
+        private static void BrandAddTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            brandManager.Add(new Brand { BrandName = "Fiat" });
+            brandManager.Add(new Brand { BrandName = "Renault" });
+        }
+
+        private static void UserAddTest()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+            userManager.Add(new User { FirstName = "Gizem", LastName = "Yaman", Email = "gizem@gmail.com", Password = "12345" });
+            userManager.Add(new User { FirstName = "Kerem", LastName = "Yaman", Email = "kerem@gmail.com", Password = "12345" });
+            userManager.Add(new User { FirstName = "Alparslan", LastName = "Tekkanat", Email = "alp@gmail.com", Password = "12345" });
         }
 
         private static void BrandGetAllTest()
@@ -84,9 +115,14 @@ namespace ConsoleUI
         private static void CarAddTest()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Car { CarName = "Volkswagen Jetta", BrandId = 1, ColorId = 2, ModelYear = "2017", 
-                DailyPrice = 210, Description = "Binek Volkswagen Jetta 2017 model Beyaz - 210 TL" });
-        }
+              
+
+            carManager.Add(new Car {CarName = "Volkswagen Caddy", BrandId = 1, ColorId = 3, ModelYear = "2018", DailyPrice = 220, Description = "Ticari Volkswagen Caddy 2018 model Gri - 220 TL" });
+            carManager.Add(new Car {CarName = "Fiat Doblo", BrandId = 4, ColorId = 4, ModelYear = "2018", DailyPrice = 220, Description = "Ticari Fiat Doblo 2018 model Kırmızı - 220 TL" });
+            carManager.Add(new Car {CarName = "Renault Kangoo", BrandId = 5, ColorId = 1, ModelYear = "2019", DailyPrice = 230, Description = "Ticari Renault Kangoo  2019 model Siyah - 230 TL" });
+
+
+            }
 
         private static void BrandGetByIdTest()
         {
